aad2d271fc348c1021d46e76bf58a88d3f486ae9
efra-tech <eahsan@uw.edu>
2023-04-30 23:53:03 -0700

    firebase and final css

--- atlas-webpage/src/App.js
+++ atlas-webpage/src/App.js
@@ -12,9 +12,9 @@ export default function App(props) {
             <p> </p>
             <div>
               <h1>Unlock your digital campus.</h1>
-              <p className='vision-stmt' style={{color:"#777575", fontSize: '0.9rem', paddingLeft: '3%'}}>Atlas digitizes your college campus by bringing local businesses, clubs, and alumni right to your phone.</p>
+              <p className='vision-stmt' style={{color:"#777575", fontSize: '0.9rem', paddingLeft: '3%', textAlign: 'center'}}>Atlas digitizes your college campus by bringing local businesses, clubs, and alumni right to your phone.</p>
               <div className='btn-container'>
-                <div className='btn-headtitle pb-3 pt-1' style={{color:"#9b9999", fontSize: '0.8rem', paddingLeft: '3%'}}>Want exlusive access?</div>
+                <div className='btn-headtitle pb-3 pt-1' style={{color:"#9b9999", fontSize: '0.8rem', paddingLeft: '3.3%'}}>Want exlusive access?</div>
                 <a rel="noreferrer" href='https://forms.gle/qV3QL2AMqX5jgoW67' target='_blank' ><button className='btn'>Join the Waitlist</button></a>
               </div>
             </div>
--- atlas-webpage/src/index.js
+++ atlas-webpage/src/index.js
@@ -7,6 +7,19 @@ import 'bootstrap/dist/js/bootstrap.min.js';
 import './index.css';
 import App from './App';
 
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA4nwc_S6RVF-fTgyT0b34q_3M_ZDLDlMs",
+  authDomain: "atlas-25a78.firebaseapp.com",
+  projectId: "atlas-25a78",
+  storageBucket: "atlas-25a78.appspot.com",
+  messagingSenderId: "141827458782",
+  appId: "1:141827458782:web:312a49ae6b291e0aaea44f"
+};
+
+const app = initializeApp(firebaseConfig);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
